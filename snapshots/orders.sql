a1942d0c4852504f99ece1837d144a84755623fd
Jordy Sels <jordy.sels@ordina.be>
2024-06-28 08:44:13 +0000

    commit orders

---
+++ snapshots/orders.sql
@@ -0,0 +1,18 @@
+{#
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_database='analytics',
+      target_schema='snapshots',
+      unique_key='id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from {{ref ('orders') }}
+
+{% endsnapshot %}
+#}
